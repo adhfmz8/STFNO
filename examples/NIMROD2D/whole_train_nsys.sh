b03e93460eeb7e5123d0bfffdd31c9c15115bc02
adhfmz8 <73725651+adhfmz8@users.noreply.github.com>
2025-11-05 17:51:05 -0700

    Update whole_train_nsys.sh

--- examples/NIMROD2D/whole_train_nsys.sh
+++ examples/NIMROD2D/whole_train_nsys.sh
@@ -19,7 +19,7 @@ module load pytorch
 
 cd $SCRATCH
 
-NSYS_REPORT_FILE="fno_model_profile.nsys-rep"
+NSYS_REPORT_FILE="fno_model_profile_jit.nsys-rep"
 
 export PYTHONPATH=$SLURM_SUBMIT_DIR/../../:$PYTHONPATH
 
